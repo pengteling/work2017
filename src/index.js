c57a6e0f560d15dc45f3d42f975528e6f4dfaa8f
pengteling <pengteling@gmail.com>
2018-01-23 18:07:48 +0800

    20180123

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-if (process.env.NODE_ENV !== 'production') { //开发环境下 raw-loader  html文件 动态加载
-    require('./index.html');    
-};
-require("./sass/style.scss");
-// require("jquery");
-//require("./js/pJqueryAppearAnimateCSS3/jac.js"); //动画
-//require("./js/superslide/jquery.SuperSlide.2.1.2.js");
-require("./js/superslide/jquery.SuperSlide.2.1.1.js");
 
-$(function(){
-	$(".slider").slide({mainCell:".bd ul",effect:"fold",autoPlay:true,interTime:5000});
-})
+if (process.env.NODE_ENV !== 'production') { //开发环境下 raw-loader  html文件 动态加载
+    require('./index.html');
+    
+}
+;
+require("./sass/style.scss");
+var $ = require("jquery");
+//require("./js/pJqueryAppearAnimateCSS3/jac.js"); //动画
+$(function() {
+	function resize() {
+	    var pr = ($(window).width()) / 750;
+	    if(pr>1)pr=1;
+	    $("html").css("font-size", 625 * pr + "%");	    
+	}
+	resize();
+	$(window).load(function() {
+	    resize();
+	})
+	$(window).resize(function() {
+	    resize();
+	});
+	//alert("Test")
+});
